19c3118d7a9d6a760844881ad1a31f2152785caf
Apurbo Kumar Anup <apurboka@gmail.com>
2023-03-21 22:06:00 +0600

    Update

---
+++ src/components/common/RoomCard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const RoomCard = ({data}) => {
+  return (
+    <div className="col-md-3 mb-3" key={data.id}>
+          <div className="card p-2 " key={data.id}>
+            <img
+              src={data.image}
+              className="card-img-top"
+              alt={data.title}
+              height="250px"
+            />
+            <div className="card-body">
+              <h5 className="card-title lead fw-bold mb-0">{data.title}</h5>
+              <p className="card-text ">{data.description}</p>
+            </div>
+          </div>
+        </div>
+  )
+}
+
+export default RoomCard
---
+++ src/components/json/RoomData.js
@@ -0,0 +1,42 @@
+export const RoomData = [
+    {
+      id: 1,
+      title: "Simyacı",
+      description: "Paulo Coelho",
+      fare: 9.99,
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/51eqjXwFzwL._SX344_BO1,204,203,200_.jpg",
+    },
+    {
+      id: 2,
+      title: "Yüzyıllık Yalnızlık",
+      description: "Gabriel García Márquez",
+      fare: 19.99,
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/51A1ZxGf94L._SY291_BO1,204,203,200_QL40_ML2_.jpg",
+    },
+    {
+      id: 3,
+      title: "Annemi Bir Kez Daha Görebilsem",
+      description: "Zana Muhsen",
+      fare: 16.99,
+      image:
+        "https://content.babil.com/urun/13df0fdbdfbe477d8c8ee30d2e15292c/Front/Big",
+    },
+    {
+      id: 4,
+      title: "Ekmek Kavgası",
+      description: "Orhan Kemal",
+      fare: 19.99,
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/41BJdviD3TL._SX340_BO1,204,203,200_.jpg",
+    },
+    {
+      id: 5,
+      title: "Aşk",
+      description: "Elif Şafak",
+      fare: 25.99,
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/414N7XbRE%2BL._SX344_BO1,204,203,200_.jpg",
+    },
+  ];
