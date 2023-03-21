1b7d656fb3d0c0f8ca33bb69dc5ece89f0e8558a
Apurbo Kumar Anup <apurboka@gmail.com>
2023-03-21 23:14:53 +0600

    Update

--- src/components/views/HomePage.js
+++ src/components/views/HomePage.js
@@ -11,19 +11,7 @@ const HomePage = () => {
     <>
       <Carousel />
 
-      <div className="hero">
-        <div className="card text-bg-warning">
-          <img src="/assets/bg.png" className="card-img" alt="/" height={500} />
-          <div className="card-img-overlay d-flex flex-column justify-content-around">
-            <div className="container">
-              <h5 className="card-title display-3 text-mute fw-bolder mb-0">
-                NEW SESSION ARRIVALS
-              </h5>
-              <p className="card-text lead fs-2">CHECK OUT ALL THE TRENDS</p>
-            </div>
-          </div>
-        </div>
-      </div>
+      
 
       <div className="container main-body-width my-4">
         <div className="row pt-3 d-flex justify-content-between">
--- src/components/views/RoomPage.js
+++ src/components/views/RoomPage.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import RoomCard from '../common/RoomCard'
+import { RoomData } from '../json/RoomData'
 
 const RoomPage = () => {
   return (
-    <div>RoomPage</div>
+    <>
+      
+      <div className="hero">
+        <div className="card text-bg-warning">
+          <img src="/assets/bg.png" className="card-img" alt="/" height={500} />
+          <div className="card-img-overlay d-flex flex-column justify-content-around">
+            <div className="container">
+              <h5 className="card-title display-3 text-mute fw-bolder mb-0">
+                NEW SESSION ARRIVALS
+              </h5>
+              <p className="card-text lead fs-2">CHECK OUT ALL THE TRENDS</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container main-body-width my-4">
+        <div className="row pt-3 d-flex justify-content-between">
+          <div className="row post-main">
+            {RoomData.map((data) => (
+              <RoomCard key={data.id} data={data} />
+            ))}
+          </div>
+        </div>
+      </div>
+
+
+    </>
   )
 }
 
