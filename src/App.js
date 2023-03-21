215d12283f17b1e74963c07998fd15c34522e4e1
Apurbo Kumar Anup <apurboka@gmail.com>
2023-03-21 21:39:36 +0600

    HomePage.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/layout/include/Navbar";
+import MasterLayout from "./components/layout/MasterLayout";
+import HomePage from "./components/views/HomePage";
+import RoomPage from "./components/views/RoomPage";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<MasterLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="cart" element={<RoomPage />} />
+        </Route>
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/layout/MasterLayout.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Outlet } from "react-router-dom";
+const MasterLayout = () => {
+  return (
+    <Outlet />
+  )
+}
+
+export default MasterLayout
